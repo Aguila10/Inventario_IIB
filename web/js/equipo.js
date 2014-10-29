79acd7f1e85be74538181a2efe131d7deafd0ed8
jpachecov <la.parte.imaginaria@gmail.com>
2014-10-28 20:59:00 -0600

    ya muestra los html que tenemos

---
+++ web/js/equipo.js
@@ -0,0 +1,77 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+function validaCampos(){
+    
+    var reg = "[0-9]+$";
+    
+    var claveAF = document.forms["alta"]["activoFijo"].value;
+    var numInvUNAM = document.forms["alta"]["descripcion"].value;
+    var descripcion = document.forms["alta"]["descripcionExtendida"].value;
+    
+    if(claveAF == "" || claveAF == null){
+        if(numInvUNAM == "" || numInvUNAM == null){
+            //alert("Debes meter alguno de los campos");
+            document.getElementById("errorActivoFijo").innerHTML="campo inválido";
+            document.getElementById("errorDescripcion").innerHTML="campo inválido";
+            document.getElementById("errorDescripcionExtendida").innerHTML="";
+            return false;
+        }
+        // claveAF = null y numInvUNAM != null
+    var con = numInvUNAM.match(reg);
+    if(con == null){
+            document.getElementById("errorDescripcion").innerHTML="Este campo debe ser un numero";
+            return false;
+    }
+
+ 
+        if (descripcion == null || descripcion == ""){
+            document.getElementById("errorDescripcionExtendida").innerHTML="Este campo no debe ser vacío";
+            return false;
+        
+        }
+    return true;
+    }
+
+    //claveAF != null
+    
+    //checar si claveAF es numero
+
+        var con = claveAF.match(reg);
+    if(con == null){
+            document.getElementById("errorActivoFijo").innerHTML="Este campo debe ser un numero";
+            return false;
+    }
+    if(numInvUNAM != ""){
+        var r = numInvUNAM.match(reg)
+            if(r == null){
+            document.getElementById("errorDescripcion").innerHTML="Este caaaampo debe ser un numero";
+            return false;
+    }
+    }
+    
+    if (descripcion == null || descripcion == ""){
+            document.getElementById("errorDescripcionExtendida").innerHTML="Este campo no debe ser vacío";
+            return false;
+        }
+    
+    return true;
+    
+}
+
+function buscaEquipo() {
+    
+    
+    var x = document.getElementById("campoBusqueda").value;
+    $.post("BuscaEquipo", {
+		campoBusqueda : x
+	},function(data){
+		$("#resultadoBusqueda").html(data);
+	});
+    
+}
